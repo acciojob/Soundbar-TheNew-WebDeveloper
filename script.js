3b45f6e432bb6e5dd3ce785588c415ee64c8b77b
Mohammed_Rizwan <160338674+TheNew-WebDeveloper@users.noreply.github.com>
2025-07-17 12:59:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-//your JS code here. If required.
+var btn = document.querySelector("audio");
+let myButtons = document.querySelectorAll(".btn");
+
+function play(event) {
+    const val = event.target;
+    btn.src = (`sounds/${val.textContent}.mp3`);
+    btn.play();
+}
+
+function stop() {
+    btn[0].pause();
+}
+
+myButtons.forEach(button => {
+    button.addEventListener('click', play);
+});
+document.querySelector(".stop").addEventListener("click", stop);
